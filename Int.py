1ea90517ee78cc97d5c664f77cb7c602e5db3a13
nheit-69 <149867800+nheit-69@users.noreply.github.com>
2024-08-01 18:51:37 +0530

    Integer
    Topic - Number
    - Integer

---
+++ Int.py
@@ -0,0 +1,43 @@
+   # python Data 5 Types
+
+#Number Data three type
+  # 1.Int
+  # 2.float
+  # 3.complex
+
+  #int mean number
+prem = 54
+print(prem)
+print(type(prem))
+
+jet = +844
+print(jet)
+print(type(jet))
+
+j = -8
+print(j)
+print(type(j))
+
+kz = 8+9
+print(kz)
+print(type(kz))
+
+kz = 90-9
+print(kz)
+print(type(kz))
+
+kz = 89-110
+print(kz)
+print(type(kz))
+
+kz = 8*9
+print(kz)
+print(type(kz))
+
+kz = 4%9
+print(kz)
+print(type(kz))
+
+kz = 8^9
+print(kz)
+print(type(kz))
